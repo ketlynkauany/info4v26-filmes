d19d2617cb2c4b15ff768c959d5af5919b0e6566
ketlynkauany <ketlynlove13@gmail.com>
2026-05-26 23:51:56 -0300

    atividade media final

--- src/app/calcula-media/calcula-media.ts
+++ src/app/calcula-media/calcula-media.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calcula-media',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calcula-media.html',
-  styleUrl: './calcula-media.scss',
+  styleUrl: './calcula-media.scss'
 })
 export class CalculaMedia {
-  protected mediaParcial: number | undefined
-  
-  constructor() {
-    this.mediaParcial = undefined
-  }
-  
 
-  calcularMediaParcial(b1: number, b2: number, 
-                       b3: number, b4: number) {
-      this.mediaParcial = 
-        (b1 * 2 + b2 * 2 + b3 * 3 + b4 * 3) / 10
+  nota1: number = 0;
+  nota2: number = 0;
+  notaAvFinal: number = 0;
+
+  mediaParcial: number = 0;
+  mediaFinal: number = 0;
+
+  situacao: string = '';
+
+  calcularMedia() {
+
+    this.mediaParcial = (this.nota1 + this.nota2) / 2;
+
+    this.mediaFinal = (this.mediaParcial + this.notaAvFinal) / 2;
+
+    if (this.mediaFinal >= 6) {
+      this.situacao = 'Aprovado';
+    } else {
+      this.situacao = 'Reprovado';
+    }
+
   }
+
 }
